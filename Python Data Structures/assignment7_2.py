cdce3267bdcfd4d0625d1e226e9066fe993ec7b2
Vansh <168204044+Vansh-programmer@users.noreply.github.com>
2025-05-15 15:09:17 +0530

    module 4.1

--- Python Data Structures/assignment7_2.py
+++ Python Data Structures/assignment7_2.py
@@ -1,13 +1,20 @@
 #Average of floating values 
 #e.g. X-DSPAM-Confidence:    0.8475
-#file mbox-short.txt
+#file enter file name: C:\Users\vansh\OneDrive\Desktop\Python_Codes\Python Data Structures\mbox-short.txt
+
 fname = input('enter file name:')
 file_acess = open(fname)
+total = 0
+avg =0
+value = 0
 for line in file_acess:
     if not line.startswith('X-DSPAM-Confidence:'):
         continue
     else:
-        print('find' , line.find('0.'))
-        
+        line.find('0.')
+    value+=1
+    data = float(line[20:])
+    total +=data
 
-    print('line',line)
+avg = total/value
+print(avg)
